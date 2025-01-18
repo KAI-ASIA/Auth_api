f5e3b5f68de191bf49baac7162d5dfd354a67a61
Nguyen Tuan Vinh Ninh K17 HL <ninhntvhe172162@fpt.edu.vn>
2025-01-18 18:29:49 +0700

    Auth_api/dao/imp/AuthOTPDao update: update table name of 2 method getOTP and setConfirmTime; resources: spring-beans-dao.xml: add new bean

--- src/main/java/com/kaiasia/app/service/Auth_api/dao/imp/AuthOTPDao.java
+++ src/main/java/com/kaiasia/app/service/Auth_api/dao/imp/AuthOTPDao.java
@@ -27,8 +27,8 @@ public class AuthOTPDao extends CommonDAO implements IAuthOTPDao {
     PosgrestDAOHelper posgrestDAOHelper;
 
     @Override
-    public OTP getOTP(String sessionId, String username, String transId ) throws Exception {
-        String sql = "SELECT * FROM auth_api.otp " +
+    public OTP getOTP(String sessionId, String username, String transId) throws Exception {
+        String sql = "SELECT * FROM " + this.getTableName() + " " +
                 "where auth_api.otp.username = :username and auth_api.otp.session_id = :session_id " +
                 "and auth_api.otp.trans_id = :trans_id;";
         Map<String, String> params = new HashMap<>();
@@ -41,8 +41,8 @@ public class AuthOTPDao extends CommonDAO implements IAuthOTPDao {
     }
 
     @Override
-    public void setConfirmTime( Timestamp now, OTP otp) throws Exception {
-        String sql = "UPDATE auth_api.otp SET status='CONFIRMED', confirm_time= :confirm_time " +
+    public void setConfirmTime(Timestamp now, OTP otp) throws Exception {
+        String sql = "UPDATE "+ this.getTableName() +" SET status='CONFIRMED', confirm_time= :confirm_time " +
                 "WHERE auth_api.otp.username = :username and auth_api.otp.session_id = :session_id " +
                 "and auth_api.otp.trans_id = :trans_id ;";
         Map<String, Object> params = new HashMap<>();
@@ -50,7 +50,7 @@ public class AuthOTPDao extends CommonDAO implements IAuthOTPDao {
         params.put("session_id", otp.getSession_id());
         params.put("trans_id", otp.getTrans_id());
         params.put("confirm_time", now);
-        posgrestDAOHelper.update(sql,params);
+        posgrestDAOHelper.update(sql, params);
     }
 
     @Override
@@ -63,7 +63,7 @@ public class AuthOTPDao extends CommonDAO implements IAuthOTPDao {
         params.put("username", body.get("username"));
         params.put("trans_id", body.get("trans_id"));
 
-        OTP lastestOtp = posgrestDAOHelper.querySingle(sql,params,new BeanPropertyRowMapper<>(OTP.class));
+        OTP lastestOtp = posgrestDAOHelper.querySingle(sql, params, new BeanPropertyRowMapper<>(OTP.class));
         if (lastestOtp != null && lastestOtp.getValidate_code().equals(body.get("validate_code"))) {
             LocalDateTime now = LocalDateTime.now();
             return lastestOtp.getEnd_time().toLocalDateTime().isBefore(now.plusMinutes(2));
@@ -72,7 +72,7 @@ public class AuthOTPDao extends CommonDAO implements IAuthOTPDao {
     }
 
     @Override
-    public int insertOTP(Auth2InsertDb auth2InsertDb) throws Exception{
+    public int insertOTP(Auth2InsertDb auth2InsertDb) throws Exception {
         String sql = "INSERT INTO auth_api.otp\n" +
                 "(validate_code, trans_id, username, channel, start_time, location, end_time, status, session_id, confirm_time, trans_info, trans_time)\n" +
                 "VALUES(:validate_code, :trans_id, :username,:channel, :start_time, :location,:end_time, :status, :session_id, :confirm_time, :trans_info, :trans_time);";
@@ -87,13 +87,12 @@ public class AuthOTPDao extends CommonDAO implements IAuthOTPDao {
         param.put("status", auth2InsertDb.getStatus());
         param.put("session_id", auth2InsertDb.getSessionId());
         param.put("confirm_time", auth2InsertDb.getConfirmTime());
-        param.put("trans_info",auth2InsertDb.getTransInfo());
-        param.put("trans_time",auth2InsertDb.getTransTime());
+        param.put("trans_info", auth2InsertDb.getTransInfo());
+        param.put("trans_time", auth2InsertDb.getTransTime());
 
         int result = posgrestDAOHelper.update(sql, param);
         return result;
     }
-
 
 
 }
